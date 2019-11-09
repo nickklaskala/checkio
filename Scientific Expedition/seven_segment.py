a8207ce85dedb33ce9ec4012d49287559a04c9ba
Nick Klaskala <nklaskala@prometheusgroup.com>
2019-11-09 16:07:36 -0500

    checkio

--- Scientific Expedition/seven_segment.py
+++ Scientific Expedition/seven_segment.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env checkio --domain=py run seven-segment
 
-# You have a device that uses aSeven-segment displayto display 2 digit numbers.However, some of the segments aren't working and can't be displayed.
-# 
-# You will be given information on the lit and broken segments.You won't know whether the broken segment is lit or not.You have to count and return the total number that the device may be displaying.
-# The input is a set of lit segments (the first argument) and broken segments (the second argument).
-# Uppercase letters represent the segments of the first out two digit number.Lowercase letters represent the segments of the second out two digit number.topmost: 'A(a)', top right: 'B(b)', bottom right: 'C(c)', bottommost: 'D(d)', bottom left: 'E(e)', top left: 'F(f)', middle: 'G(g)'
-# Input:Two arguments. The first one contains the lit segments as a set of letters representing segments. The second one contains the broken segments as a set of letters representing segments.
-# Output:The total number that the device may be displaying.
-# Precondition:
-# all(re.match('[A-Ga-g]', s) for s in lit | broken)len(lit  &  broken) == 0
-# END_DESC
 
 def seven_segment(lit_seg, broken_seg):
 	up_lit_seg=[c for c in lit_seg if c.isupper()]
@@ -21,17 +11,10 @@ def seven_segment(lit_seg, broken_seg):
 	upNumMap={00:['A','B','C','D','E','F'], 10:['B','C'], 20:['A','B','G','E','D'], 30:['A','B','G','C','D'], 40:['F','G','B','C'], 50:['A','F','G','C','D'], 60:['A','F','G','C','D','E'], 70:['A','B','C'], 80:['A','B','C','D','E','F','G'], 90:['A','B','C','D','F','G'], }
 	lwNumMap={0 :['a','b','c','d','e','f'], 1 :['b','c'], 2 :['a','b','g','e','d'], 3 :['a','b','g','c','d'], 4 :['f','g','b','c'], 5 :['a','f','g','c','d'], 6 :['a','f','g','c','d','e'], 7 :['a','b','c'], 8 :['a','b','c','d','e','f','g'], 9 :['a','b','c','d','f','g'], }
 
-	lw,up,m=[],[],[]
-	for num,nset in upNumMap.items():
-		if set(up_lit_seg).issubset(nset) and set(nset).issubset(up_tot_seg):
-			up.append(num)
-	for num,nset in lwNumMap.items():
-		if set(lw_lit_seg).issubset(nset) and set(nset).issubset(lw_tot_seg):
-			lw.append(num)
+	up=[num for num,nset in upNumMap.items() if set(up_lit_seg).issubset(nset) and set(nset).issubset(up_tot_seg)]
+	lw=[num for num,nset in lwNumMap.items() if set(lw_lit_seg).issubset(nset) and set(nset).issubset(lw_tot_seg)]
 
-	for n in up:
-		for l in lw:
-			m.append(n+l)
+	m=[u+l for u in up for l in lw]
 
 	print(m)
 	return len(m)
