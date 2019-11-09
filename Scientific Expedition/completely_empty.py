d18e4b5e2470bd91181fe6a381a774ccd4d7eaa1
Nick Klaskala <nklaskala@prometheusgroup.com>
2019-11-09 08:08:03 -0500

    checkio

--- Scientific Expedition/completely_empty.py
+++ Scientific Expedition/completely_empty.py
@@ -1,41 +1,50 @@
 #!/usr/bin/env checkio --domain=py run completely-empty
 
-def completely_empty(val):
-	print(val)
-	if len(str(val))==0:
-		return True
-	elif type(val)==dict:
-		if not all(completely_empty(k) for k,v in val.items()) :
-			print('Falsedict')
-			return False
-		else:
-			print('trueDict')
-			return True
-	elif type(val)==list:
-		if not all(completely_empty(v) for v in val) :
-			print('Falselist')
-			return False
-		else:
-			print('trueList')
-			return True
-	elif len(str(val))>0:
-		print('falseVal')
+def test(val):
+	test(val)
+def completely_empty(iterable):
+	try:
+		return all(completely_empty(i) for i in iterable)
+	except:
 		return False
-	else:
-		print('truVal')
-		return True
 
-completely_empty([iter(())])
+
+
+
+
+
+# completely_empty([0])
+# x=completely_empty(['3'])
+# print (x)
+
+
+# def func(val):
+# 	val+1
+# x=[]
+
+# def func2(val):
+# 	try:
+# 		print('hi')
+# 		func(val)
+# 	except:
+# 		print('except')
+
+# v=func2(x)
+# print(v)
+
+
+
+
 
 if __name__ == '__main__':
 	#These "asserts" using only for self-checking and not necessary for auto-testing
+	assert completely_empty(['']) == True
 	assert completely_empty([]) == True, "First"
 	assert completely_empty([1]) == False, "Second"
 	assert completely_empty([[]]) == True, "Third"
 	assert completely_empty([[],[]]) == True, "Forth"
 	assert completely_empty([[[]]]) == True, "Fifth"
 	assert completely_empty([[],[1]]) == False, "Sixth"
-	assert completely_empty(['']) == True
 	assert completely_empty([0]) == False, "[0]"
 	assert completely_empty([[],[{'':'No WAY'}]]) == True
 	print('Done')
