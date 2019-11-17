aa5129516c60c2ec2e1d918e9a80e3cc59b87588
Nick Klaskala <nklaskala@prometheusgroup.com>
2019-11-17 15:13:08 -0500

    checkio

--- O'Reilly/ghosts_age.py
+++ O'Reilly/ghosts_age.py
@@ -28,14 +28,24 @@
 # 
 # END_DESC
 
-def checkio(opacity):
-    return 0
+def checkio(measurement,ageMax=10000):
+	fib,a,b=[],0,1
+	while a < ageMax:
+		fib.append(a)
+		b,a=a,a+b
+
+	opacity,dct=ageMax,{ageMax:0}
+	for yr in range(ageMax):
+		opacity+= -yr if yr in fib else +1
+		dct[opacity]=yr
+	return dct[measurement]
+
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
-    assert checkio(10000) == 0, "Newborn"
-    assert checkio(9999) == 1, "1 year"
-    assert checkio(9997) == 2, "2 years"
-    assert checkio(9994) == 3, "3 years"
-    assert checkio(9995) == 4, "4 years"
-    assert checkio(9990) == 5, "5 years"
+	assert checkio(10000) == 0, "Newborn"
+	assert checkio(9999) == 1, "1 year"
+	assert checkio(9997) == 2, "2 years"
+	assert checkio(9994) == 3, "3 years"
+	assert checkio(9995) == 4, "4 years"
+	assert checkio(9990) == 5, "5 years"
