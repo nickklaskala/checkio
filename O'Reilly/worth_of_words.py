38dba4af080fad682f76f67916d3fe7cf060304c
Nick Klaskala <nklaskala@prometheusgroup.com>
2019-12-17 08:16:46 -0500

    checkio

--- O'Reilly/worth_of_words.py
+++ O'Reilly/worth_of_words.py
@@ -29,20 +29,32 @@
 # END_DESC
 
 VALUES = {'e': 1,  'a': 1, 'i': 1, 'o': 1, 'n': 1, 'r': 1,
-          't': 1,  'l': 1, 's': 1, 'u': 1, 'd': 2, 'g': 2,
-          'b': 3,  'c': 3, 'm': 3, 'p': 3, 'f': 4, 'h': 4,
-          'v': 4,  'w': 4, 'y': 4, 'k': 5, 'j': 8, 'x': 8,
-          'q': 10, 'z': 10}
+		  't': 1,  'l': 1, 's': 1, 'u': 1, 'd': 2, 'g': 2,
+		  'b': 3,  'c': 3, 'm': 3, 'p': 3, 'f': 4, 'h': 4,
+		  'v': 4,  'w': 4, 'y': 4, 'k': 5, 'j': 8, 'x': 8,
+		  'q': 10, 'z': 10}
+
 
 def worth_of_words(words):
-    #replace this for solution
-    return words
+	result=('',0)
+	for word in words:
+		data=[]
+		for l in word:
+			data.append(VALUES[l])
+		length=eval(('+'.join(map(str, data))))
+		if length>result[1]:
+			result=(word,length)
+	return result[0]
+
+
+
+
 
 if __name__ == '__main__':
-    print("Example:")
-    print(worth_of_words(['hi', 'quiz', 'bomb', 'president']))
+	print("Example:")
+	print(worth_of_words(['hi', 'quiz', 'bomb', 'president']))
 
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert worth_of_words(['hi', 'quiz', 'bomb', 'president']) == 'quiz'
-    assert worth_of_words(['zero', 'one', 'two', 'three', 'four', 'five']) == 'zero'
-    print("Coding complete? Click 'Check' to earn cool rewards!")
+	#These "asserts" using only for self-checking and not necessary for auto-testing
+	assert worth_of_words(['hi', 'quiz', 'bomb', 'president']) == 'quiz'
+	assert worth_of_words(['zero', 'one', 'two', 'three', 'four', 'five']) == 'zero'
+	print("Coding complete? Click 'Check' to earn cool rewards!")
