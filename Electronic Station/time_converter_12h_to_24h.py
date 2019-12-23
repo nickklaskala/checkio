b30c744a0c66de95e7f16d1c2aed57b8caafdda5
Nick Klaskala <nklaskala@prometheusgroup.com>
2019-12-23 08:06:36 -0500

    checkio

--- Electronic Station/time_converter_12h_to_24h.py
+++ Electronic Station/time_converter_12h_to_24h.py
@@ -18,15 +18,22 @@
 # END_DESC
 
 def time_converter(time):
-    #replace this for solution
-    return time
+	p=11
+	t,a=time.split(' ')
+	
+	return time
+
+
+	='12:30 p.m.'
+	p=11
+	t,a=time.split(' ')
 
 if __name__ == '__main__':
-    print("Example:")
-    print(time_converter('12:30 p.m.'))
+	print("Example:")
+	print(time_converter('12:30 p.m.'))
 
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert time_converter('12:30 p.m.') == '12:30'
-    assert time_converter('9:00 a.m.') == '09:00'
-    assert time_converter('11:15 p.m.') == '23:15'
-    print("Coding complete? Click 'Check' to earn cool rewards!")
+	#These "asserts" using only for self-checking and not necessary for auto-testing
+	assert time_converter('12:30 p.m.') == '12:30'
+	assert time_converter('9:00 a.m.') == '09:00'
+	assert time_converter('11:15 p.m.') == '23:15'
+	print("Coding complete? Click 'Check' to earn cool rewards!")
