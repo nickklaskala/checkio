84c02197ac59633951a79db1a09beba03201a994
Nick Klaskala <nklaskala@prometheusgroup.com>
2019-12-24 11:25:11 -0500

    checkio

--- Electronic Station/the_longest_palindromic.py
+++ Electronic Station/the_longest_palindromic.py
@@ -14,18 +14,34 @@
 # 
 # END_DESC
 
-def longest_palindromic(a):
-    # your code here
-    return None
+from itertools import product
+def longest_palindromic(line):
+	line=''.join(reversed(line))
+	k=range(len(line)+1)
+	longest=''
+	
+	for h,i in product(k,k):
+		text=line[i:h]
+		if len(text)==0:
+			continue
+		if  all([text[i]==text[~i] for i in range(len(text))]) and\
+			len(text)>=len(longest):
+			longest=text
+	print(longest)
+	return longest
+
+
+
+longest_palindromic('abjncqdkcmabjncqdkcmabjncqdkcmabjncqdkcmabjncqdkcmabjncqdkcmabjncqdkcmabjncqdkcmmckdqcnjbamckdqcnjbamckdqcnjbamckdqcnjbamckdqcnjbamckdqcnjbamckdqcnjbamckdqcnjba;samca;sdkmc;akdsm;kds;oiolkjhgfdsasdfghjklyuweabjncqdkcm;samca;sdkmc;akdsm;kds;oioyuweabjncqdkcm;samca;sdkmc;akdsm;kds;oioyuweabjncqdkcm;samca;sdkmc;akdsm;kds;oioyuweabjncqdkcm;samca;sdkmc;akdsm;kds;oioyuweabjncqdkcm;samca;sdkmkjaslkhasodiuhwennrwmenf,dsncuindsdsncuindsc')
 
 
 if __name__ == '__main__':
-    print("Example:")
-    print(longest_palindromic('abc'))
+	print("Example:")
+	print(longest_palindromic('abc'))
 
-    # These "asserts" are used for self-checking and not for an auto-testing
-    assert longest_palindromic('abc') == 'a'
-    assert longest_palindromic('abacada') == 'aba'
-    assert longest_palindromic('artrartrt') == 'rtrartr'
-    assert longest_palindromic('aaaaa') == 'aaaaa'
-    print("Coding complete? Click 'Check' to earn cool rewards!")
+	# These "asserts" are used for self-checking and not for an auto-testing
+	assert longest_palindromic('abc') == 'a'
+	assert longest_palindromic('abacada') == 'aba'
+	assert longest_palindromic('artrartrt') == 'rtrartr'
+	assert longest_palindromic('aaaaa') == 'aaaaa'
+	print("Coding complete? Click 'Check' to earn cool rewards!")
