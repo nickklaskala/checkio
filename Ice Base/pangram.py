d5dedede9b2dcb3ee6c37a25300419d7b58cd579
Nick Klaskala <nklaskala@prometheusgroup.com>
2020-01-04 20:39:14 -0500

    Ice Base/pangram

--- Ice Base/pangram.py
+++ Ice Base/pangram.py
@@ -15,15 +15,11 @@
 # END_DESC
 
 def check_pangram(text):
-    '''
-        is the given text is a pangram.
-    '''
-    # your code here
-    return True or False
+	return all([(i in text.lower()) for i in 'abcdefghijklmnopqrstuvwxyz'])
 
 if __name__ == '__main__':
-    # These "asserts" using only for self-checking and not necessary for auto-testing
-    assert check_pangram("The quick brown fox jumps over the lazy dog."), "brown fox"
-    assert not check_pangram("ABCDEF"), "ABC"
-    assert check_pangram("Bored? Craving a pub quiz fix? Why, just come to the Royal Oak!"), "Bored?"
-    print('If it is done - it is Done. Go Check is NOW!')
+	# These "asserts" using only for self-checking and not necessary for auto-testing
+	assert check_pangram("The quick brown fox jumps over the lazy dog."), "brown fox"
+	assert not check_pangram("ABCDEF"), "ABC"
+	assert check_pangram("Bored? Craving a pub quiz fix? Why, just come to the Royal Oak!"), "Bored?"
+	print('If it is done - it is Done. Go Check is NOW!')
