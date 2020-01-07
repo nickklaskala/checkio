15453b51e9685f30501753e2d871a02fab269fd1
Nick Klaskala <nklaskala@prometheusgroup.com>
2020-01-06 21:14:24 -0500

    Ice Base/min_max

--- Ice Base/min_max.py
+++ Ice Base/min_max.py
@@ -23,21 +23,37 @@
 # END_DESC
 
 def min(*args, **kwargs):
-    key = kwargs.get("key", None)
-    return None
+	key = kwargs.get("key", lambda x: x)
+	if len(args)==1:
+		args=args[0]
+
+	args2=[key(i) for i in args]
+	rst=0
+	for i in range(len(args2)):
+		if args2[i]<args2[rst]:
+			rst=i
+	return args[rst]
 
 
 def max(*args, **kwargs):
-    key = kwargs.get("key", None)
-    return None
+	key = kwargs.get("key", lambda x: x)
+	if len(args)==1:
+		args=args[0]
+
+	args2=[key(i) for i in args]
+	rst=0
+	for i in range(len(args2)):
+		if args2[i]>args2[rst]:
+			rst=i
+	return args[rst]
 
 
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert max(3, 2) == 3, "Simple case max"
-    assert min(3, 2) == 2, "Simple case min"
-    assert max([1, 2, 0, 3, 4]) == 4, "From a list"
-    assert min("hello") == "e", "From string"
-    assert max(2.2, 5.6, 5.9, key=int) == 5.6, "Two maximal items"
-    assert min([[1, 2], [3, 4], [9, 0]], key=lambda x: x[1]) == [9, 0], "lambda key"
-    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+	#These "asserts" using only for self-checking and not necessary for auto-testing
+	assert max(3, 2) == 3, "Simple case max"
+	assert min(3, 2) == 2, "Simple case min"
+	assert max([1, 2, 0, 3, 4]) == 4, "From a list"
+	assert min("hello") == "e", "From string"
+	assert max(2.2, 5.6, 5.9, key=int) == 5.6, "Two maximal items"
+	assert min([[1, 2], [3, 4], [9, 0]], key=lambda x: x[1]) == [9, 0], "lambda key"
+	print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
