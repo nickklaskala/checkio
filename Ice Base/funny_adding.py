7b88337446f7943832ee1e8a3f189c3535461729
Nick Klaskala <nklaskala@prometheusgroup.com>
2020-01-06 21:14:10 -0500

    Ice Base/funny_adding

--- Ice Base/funny_adding.py
+++ Ice Base/funny_adding.py
@@ -10,8 +10,7 @@
 # END_DESC
 
 def checkio(data):
-    """The sum of two integer elements"""
-    a, b = data
+	return (sum(data))
     
 if __name__ == '__main__':
     assert checkio([5, 5]) == 10, 'First'
