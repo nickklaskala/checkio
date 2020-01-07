8c7519960f7d8e1031e4e82aa4ff9b5932caf2d5
Nick Klaskala <nklaskala@prometheusgroup.com>
2020-01-06 21:13:34 -0500

    Ice Base/cut_sentance

--- Ice Base/cut_sentence.py
+++ Ice Base/cut_sentence.py
@@ -18,17 +18,25 @@
 # 
 # END_DESC
 
-def cut_sentence(line, length):
-    '''
-    Cut a given sentence, so it becomes shorter than or equal to a given length.
-    '''
-    # your code here
-    return line[:length]
+
+
+
+
+def cut(line, length):
+	indexes=[i for i in range(len(line)) if line[i]==' ']+[len(line)]
+	rst=line
+	for loc in reversed(indexes):
+		rst=line[:loc]+'...'
+		if loc<=length:
+			break
+	return rst[:len(line)]
+
+
 
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert cut_sentence("Hi my name is Alex", 4) == "Hi...", "First"
-    assert cut_sentence("Hi my name is Alex", 8) == "Hi my...", "Second"
-    assert cut_sentence("Hi my name is Alex", 18) == "Hi my name is Alex", "Third"
-    assert cut_sentence("Hi my name is Alex", 20) == "Hi my name is Alex", "Fourth"
-    print('Done! Do you like it? Go Check it!')
+	#These "asserts" using only for self-checking and not necessary for auto-testing
+	assert cut("Hi my name is Alex", 4) == "Hi...", "First"
+	assert cut("Hi my name is Alex", 8) == "Hi my...", "Second"
+	assert cut("Hi my name is Alex", 18) == "Hi my name is Alex", "Third"
+	assert cut("Hi my name is Alex", 20) == "Hi my name is Alex", "Fourth"
+	print('Done! Do you like it? Go Check it!')
