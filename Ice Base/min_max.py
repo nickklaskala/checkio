6e1c039be8834b4dece5f4881b145f5d36febd2e
Nick Klaskala <nklaskala@prometheusgroup.com>
2020-01-19 18:42:19 -0500

    Ice Base/min_max

--- Ice Base/min_max.py
+++ Ice Base/min_max.py
@@ -24,34 +24,22 @@
 
 def min(*args, **kwargs):
 	key = kwargs.get("key", lambda x: x)
-	if len(args)==1:
-		args=args[0]
-
-	args2=[key(i) for i in args]
-	rst=0
-	for i in range(len(args2)):
-		if args2[i]<args2[rst]:
-			rst=i
-	return args[rst]
+	if len(args) == 1:
+		args = args[0]
+	return sorted(args, key=key, reverse=False)[0]
 
 
 def max(*args, **kwargs):
 	key = kwargs.get("key", lambda x: x)
-	if len(args)==1:
-		args=args[0]
-
-	args2=[key(i) for i in args]
-	rst=0
-	for i in range(len(args2)):
-		if args2[i]>args2[rst]:
-			rst=i
-	return args[rst]
-
+	if len(args) == 1:
+		args = args[0]
+	return sorted(args, key=key, reverse=True)[0]
 
 if __name__ == '__main__':
 	#These "asserts" using only for self-checking and not necessary for auto-testing
 	assert max(3, 2) == 3, "Simple case max"
 	assert min(3, 2) == 2, "Simple case min"
+	assert min(abs(i) for i in range(-10, 10)) == 0, "generators"
 	assert max([1, 2, 0, 3, 4]) == 4, "From a list"
 	assert min("hello") == "e", "From string"
 	assert max(2.2, 5.6, 5.9, key=int) == 5.6, "Two maximal items"
