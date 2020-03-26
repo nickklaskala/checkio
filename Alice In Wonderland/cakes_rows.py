16d348f0f16d17e0070cf0c3436dc7ca504ddc78
Nick Klaskala <nklaskala@prometheusgroup.com>
2020-03-26 08:33:40 -0400

    Alice in Wonderland/cake_rows

--- Alice In Wonderland/cakes_rows.py
+++ Alice In Wonderland/cakes_rows.py
@@ -27,12 +27,21 @@
 # 
 # END_DESC
 
+
+from itertools import combinations
+
 def checkio(cakes):
-    return 0
+	colinear = lambda x, y, z:(y[0]-x[0])*(z[1]-x[1]) == (y[1]-x[1])*(z[0]-x[0])
+	rows = []
+
+	for a, z in combinations(cakes, 2):
+		line = set(tuple(p) for p in cakes if colinear(a, p, z))
+		if len(line) > 2 and line not in rows:
+			rows.append(line)
+	return len(rows)
+
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
-    assert checkio([[3, 3], [5, 5], [8, 8], [2, 8], [8, 2]]) == 2
-    assert checkio(
-        [[2, 2], [2, 5], [2, 8], [5, 2], [7, 2], [8, 2],
-         [9, 2], [4, 5], [4, 8], [7, 5], [5, 8], [9, 8]]) == 6
+	assert checkio([[3, 3], [5, 5], [8, 8], [2, 8], [8, 2]]) == 2
+	assert checkio([[2, 2], [2, 5], [2, 8], [5, 2], [7, 2], [8, 2],[9, 2], [4, 5], [4, 8], [7, 5], [5, 8], [9, 8]]) == 6
