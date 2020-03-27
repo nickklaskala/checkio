f92ff9368a796c5fa5b40f424b263e73c182b45d
Nick Klaskala <nklaskala@prometheusgroup.com>
2020-03-27 10:14:59 -0400

    Alice in Wonderland/digit_doublets

--- Alice In Wonderland/digits_doublets.py
+++ Alice In Wonderland/digits_doublets.py
@@ -4,48 +4,38 @@
 # END_DESC
 
 def checkio(numbers):
-	return []
-
-numbers=[123, 991, 323, 321, 329, 121, 921, 125, 999]
-numbers=[str(i) for i in numbers]
-start,end,midnums=numbers[0],numbers[-1],numbers[1:]
-masterlist=[]
-dct={}
-for i in numbers:
-	dct[i]=[n for n in numbers if ((1 if i[0] in n[0] else 0) + (1 if i[1] in n[1] else 0) + (1 if i[2] in n[2] else 0))==2]
+	numbers=[str(i) for i in numbers]
+	start,end=numbers[0],numbers[-1]
+	masterlist=[]
+	nodedct={i:[n for n in numbers if ((1 if i[0] in n[0] else 0) + (1 if i[1] in n[1] else 0) + (1 if i[2] in n[2] else 0))==2] for i in numbers}
 
 
-def probe(i,hard_path):
-	soft_path=hard_path
-	if hard_path[-1]==end:
-		return {'winner':'yes', 'path':hard_path}
+	def probe(i,hard_path):
+		soft_path=hard_path+[i]
+		if hard_path[-1]==end:
+			return {'winner':'yes', 'path':soft_path}
+		
+		if end in nodedct[i]:
+			masterlist.append(soft_path+[end])
+			return {'winner':'yes', 'path':soft_path+[end]}
+		if all([num in soft_path for num in nodedct[i]]):
+			return {'winner':'no' , 'path':[]}
+		else:
+			for num in nodedct[i]:
+				if num in soft_path:
+					continue
+				probe(num,soft_path)
+		return'dead'
+
+	for node in nodedct[start]:
+		tmp=probe(node,[start])
 	
-	if end in dct[i]:
-		masterlist.apped(hardpath+[end])
-		return {'winner':'yes', 'path':path+[end]}
-	if all([num in path for num in dct[i]]):
-		return {'winner':'no' , 'path':[]}
-	else:
-		for num in dct[i]:
-			if num in path:
-				continue
-			probe(num,hard_path+[num])
-	return'dead'
-
-path=[start]
-
-for i in dct[path[-1]]:
-	tmp=probe(i,path)
-	if tmp['winner']=='yes':
-		masterlist=tmp[path]
-
-
-
+	return list(map(int,min(masterlist,key=len)))
 
 
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
-if __name__ == '_x_main__':
+if __name__ == '__main__':
 	assert checkio([123, 991, 323, 321, 329, 121, 921, 125, 999]) == [123, 121, 921, 991, 999], "First"
 	assert checkio([111, 222, 333, 444, 555, 666, 121, 727, 127, 777]) == [111, 121, 127, 727, 777], "Second"
 	assert checkio([456, 455, 454, 356, 656, 654]) == [456, 454, 654], "Third, [456, 656, 654] is correct too"
